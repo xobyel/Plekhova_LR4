298f47fef0e674663bce557a25f3689b4a47df30
xobyel <plehovaulia3@gmail.com>
2025-03-24 16:19:16 +0300

    Update Plekhova_project_LR4.cpp

--- Plekhova_project_LR4.cpp
+++ Plekhova_project_LR4.cpp
@@ -9,6 +9,12 @@ using namespace std;
 int side1, side2;
 
 void Task1() {
+    cout << "Введите длину первой стороны прямоугольника: ";
+    while (!(cin >> side1) || side1 <= 0) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Ошибка! Введите положительное целое число: ";
+    }
     
 }
 
