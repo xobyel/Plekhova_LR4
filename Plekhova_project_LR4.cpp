5a100fba71a2864e0fc9eb24bc04676a8d5882e0
xobyel <plehovaulia3@gmail.com>
2025-03-24 13:29:25 +0300

    add initial code

---
+++ Plekhova_project_LR4.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <functional>
+#include <map>
+
+using namespace std;
+
+int side1, side2;
+
+
+void Task1() {
+    
+}
+
+void Task2() {
+    
+}
+
+void Task3() {
+    
+}
+
+void Task4() {
+   
+}
+
+void Task5() {
+    
+}
+
+struct MenuItem {
+    string title;
+    function<void()> action;
+};
+
+int main() {
+    map<int, MenuItem> menu = {
+        {1, {"Ввод 1-й стороны прямоугольника", Task1}},
+        {2, {"Ввод 2-й стороны прямоугольника", Task2}},
+        {3, {"Calculate of Area", Task3}},
+        {4, {"Calculate of Perimeter", Task4}},
+        {5, {"Sum of fractional and integer parts (Task 12)", Task5}}
+    };
+
+    int choice = 0;
+
+    while (true) {
+        cout << "Меню:" << endl;
+        for (const auto& item : menu) {
+            cout << "Task " << item.first << ". " << item.second.title << endl;
+        }
+        cout << "0. Выход" << endl;
+        cout << "Введите номер пункта: ";
+        cin >> choice;
+
+        if (choice == 0) {
+            cout << "© 2025 FirstName LastName" << endl;
+            break;
+        }
+        cout << endl;
+        if (menu.find(choice) != menu.end()) {
+            menu[choice].action();
+        } else {
+            cout << "Некорректный ввод.";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
