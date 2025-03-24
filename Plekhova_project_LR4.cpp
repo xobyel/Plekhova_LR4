dfa0bee3f302131aefd08b78844419664227be69
xobyel <plehovaulia3@gmail.com>
2025-03-24 17:18:41 +0300

    Plekhova_project_LR4.cpp

--- Plekhova_project_LR4.cpp
+++ Plekhova_project_LR4.cpp
@@ -8,14 +8,30 @@ int side1, side2;
 
 
 void Task1() {
+     cout << "Введите длину первой стороны прямоугольника: ";
+    while (!(cin >> side1) || side1 <= 0) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Ошибка! Введите положительное целое число: ";
+    }
     
 }
 
 void Task2() {
-    
+    cout << "Введите длину второй стороны прямоугольника: ";
+    while (!(cin >> side2) || side2 <= 0) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Ошибка! Введите положительное целое число: ";
+    }
 }
 
 void Task3() {
+    if (side1 == 0 || side2 == 0) {
+        cout << "Ошибка! Сначала введите длины сторон (пункты 1 и 2 меню)." << endl;
+        return;
+    }
+    cout << "Площадь прямоугольника: " << side1 * side2 << endl;
     
 }
 
